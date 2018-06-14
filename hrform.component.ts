690a56c59c74ed695b1df03f372436b3c6551334
HARSH KANSARA <harshkansara888@gmail.com>
2018-06-14 10:57:07 +0530

    Add files via upload

---
+++ hrform.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { hrrequests } from '../hrrequests';
+
+@Component({
+  selector: 'app-hrform',
+  templateUrl: './hrform.component.html',
+  styleUrls: ['./hrform.component.css']
+})
+export class hrformcomponent {
+
+ }
